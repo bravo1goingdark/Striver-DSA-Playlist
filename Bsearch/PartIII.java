b8af0f816ef78572af16b1ffd6f8359bf1615f51
bravo1goingdark <kumarashutosh34169@gmail.com>
2025-06-06 00:35:35 +0530

    search rotated arrays II contains duplicate

--- Bsearch/PartIII.java
+++ Bsearch/PartIII.java
@@ -27,6 +27,33 @@ public class PartIII {
         }
         return -1;
     }
+    public static boolean rotatedSearchDuplicate(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return true;
+
+            if (nums[start] == nums[mid] && nums[mid] == nums[end]){
+                start++;
+                end--;
+                continue;
+            }
+
+            // Left half is sorted
+            if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target < nums[mid]) end = mid - 1;
+                else start = mid + 1;
+            }
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[end]) start = mid + 1;
+                else end = mid - 1;
+            }
+        }
+        return false;
+    }
 
 
 }
