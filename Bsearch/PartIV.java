a1d75dfe412ca2b082cf0a2d5223e7db997bb52d
bravo1goingdark <kumarashutosh34169@gmail.com>
2025-06-06 02:14:12 +0530

    search rotated arrays II contains duplicate

---
+++ Bsearch/PartIV.java
@@ -0,0 +1,29 @@
+package Bsearch;
+
+public class PartIV {
+    public static void main(String[] args) {
+        System.out.println(findMin(new int[]{8, 3, 4, 9, 2, 1, 6, 5}));
+    }
+
+
+    public static int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        int ans = Integer.MAX_VALUE;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[start] <= nums[mid]) {
+                ans = Math.min(ans, nums[start]);
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+                ans = Math.min(ans, nums[mid]);
+            }
+        }
+        return ans;
+
+    }
+}
