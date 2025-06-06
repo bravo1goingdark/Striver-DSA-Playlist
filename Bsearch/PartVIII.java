a9186ff8ea4514f832c7796d0a8376f047644e24
bravo1goingdark <kumarashutosh34169@gmail.com>
2025-06-07 01:12:55 +0530

    nth square root of number

--- Bsearch/PartVIII.java
+++ Bsearch/PartVIII.java
@@ -4,6 +4,7 @@ public class PartVIII {
 
     public static void main(String[] args) {
         System.out.println(root(8));
+        System.out.println(nroot(27,3));
     }
     
     
@@ -17,9 +18,26 @@ public class PartVIII {
             long square = (long) mid * mid;
 
             if (square == number) return mid;
-            if (square > number) end = mid - 1;
+            else if (square > number) end = mid - 1;
             else start = mid + 1;
         }
         return end;
+    }
+
+    public static int nroot(int number , int n){
+        int start = 0;
+        int end = number / n;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+
+            long square = (long) Math.pow(mid,n);
+
+            if (square == number) return mid;
+            else if (square > number) end = mid - 1;
+            else start = mid + 1;
+        }
+
+        return -1;
     }
 }
