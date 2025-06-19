2ea6b42627f0c1fc84d8f93e6bb49e090f14c31a
bravo1goingdark <kumarashutosh34169@gmail.com>
2025-06-19 18:38:52 +0530

    combination sum and letter case permutation

---
+++ Recursion/PartVII.java
@@ -0,0 +1,58 @@
+package Recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PartVII {
+    public static void main(String[] args) {
+        int[][] mat = new int[][]{
+                {1, 3, 1},
+                {1, 5, 1},
+                {4, 2, 1}
+        };
+
+        System.out.println(min(mat, mat.length - 1, mat[0].length - 1));
+
+        List<List<Integer>> ans = new ArrayList<>();
+        int[] arr = new int[]{2,3,6,7};
+        Arrays.sort(arr);
+        combSum(arr,0,7,new ArrayList<>(),ans);
+        System.out.println(ans);
+    }
+
+    public static int min(int[][] grid, int row, int col) {
+        if (row == 0 && col == 0) return grid[0][0];
+
+        if (col < 0 || row < 0) return Integer.MAX_VALUE;
+
+        int left = min(grid, row, col - 1);
+        int up = min(grid, row - 1, col);
+
+        return grid[row][col] + Math.min(left, up);
+    }
+
+    public static void combSum(int[] arr, int index, int target, List<Integer> curr, List<List<Integer>> ans) {
+        if (index >= arr.length || target < 0) return;
+        if (target == 0) {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+
+        // pick
+        if (arr[index] <= target) {
+            curr.add(arr[index]);
+            combSum(arr, index, target - arr[index], curr, ans);
+            curr.remove(curr.size() - 1);
+        }
+
+        // remove duplicates
+        int nInd = index + 1;
+        while (nInd < arr.length && arr[index] == arr[nInd]) nInd++;
+
+        // not pick
+        combSum(arr, nInd, target, curr, ans);
+    }
+
+
+}
