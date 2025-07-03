c7f4ffa56a75f29f666632da83cf83d46f4e8192
bravo1goingdark <kumarashutosh34169@gmail.com>
2025-07-03 19:46:37 +0530

    remove duplicate II

--- LinkedList/PartXVII.java
+++ LinkedList/PartXVII.java
@@ -6,19 +6,19 @@ public class PartXVII {
 
     public static void main(String[] args) {
         DoublyLinkedList<Integer> dll = new DoublyLinkedList<Integer>();
-        dll.arrToDLL(new Integer[]{1,2,3,3,4,4,5});
-        DoublyLinkedList.printDLL(removeDuplicates(dll.head));
+        dll.arrToDLL(new Integer[]{1, 2, 3, 3, 4, 4, 5});
+        DoublyLinkedList.printDLL(removeDuplicatesII(dll.head));
     }
 
-    public static DLLNode<Integer> removeDuplicates(DLLNode<Integer> head){
+    public static DLLNode<Integer> removeDuplicates(DLLNode<Integer> head) {
         if (head == null || head.next == null) return head;
 
         DLLNode<Integer> temp = head;
 
-        while (temp != null && temp.next != null){
+        while (temp != null && temp.next != null) {
             DLLNode<Integer> nextNode = temp.next;
 
-            while (nextNode != null && nextNode.value.equals(temp.value)){
+            while (nextNode != null && nextNode.value.equals(temp.value)) {
                 nextNode = nextNode.next;
             }
 
@@ -29,4 +29,38 @@ public class PartXVII {
 
         return head;
     }
+
+    public static DLLNode<Integer> removeDuplicatesII(DLLNode<Integer> head) {
+        if (head == null || head.next == null) return head;
+
+        DLLNode<Integer> dummy = new DLLNode<>(0);
+        dummy.next = head;
+        head.prev = dummy;
+
+        DLLNode<Integer> temp = head;
+
+        while (temp != null) {
+            boolean isDuplicate = false;
+            DLLNode<Integer> nextNode = temp.next;
+
+            while (nextNode != null && nextNode.value.equals(temp.value)) {
+                isDuplicate = true;
+                nextNode = nextNode.next;
+            }
+
+            if (isDuplicate) {
+                // Remove all nodes with this duplicate value
+                DLLNode<Integer> prevNode = temp.prev;
+                prevNode.next = nextNode;
+                if (nextNode != null) {
+                    nextNode.prev = prevNode;
+                }
+            } else temp = temp.next;
+
+            if (isDuplicate) temp = nextNode;
+        }
+
+        return dummy.next;
+    }
+
 }
