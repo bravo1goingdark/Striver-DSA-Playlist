b8d6c5980c6a8b8890231007aa1f22f49c919fd8
bravo1goingdark <kumarashutosh34169@gmail.com>
2025-07-08 20:28:46 +0530

    minimum bit flips

--- BitManipulation/PartIII.java
+++ BitManipulation/PartIII.java
@@ -1,4 +1,21 @@
 package BitManipulation;
 
 public class PartIII {
+    public static void main(String[] args) {
+        System.out.println(minBitFlips(10,7));
+    }
+
+
+    public static int minBitFlips(int start , int end){
+        return countSetBit(start ^ end);
+    }
+    public static int countSetBit(int num){
+        int count = 0;
+
+        while (num > 0){
+            count += num & 1;
+            num = num >> 1;
+        }
+        return count;
+    }
 }
