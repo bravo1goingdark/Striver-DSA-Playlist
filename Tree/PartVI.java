0e2b4f729ad9a06f5430219d7426906f34f91ac2
Ashutosh Kumar <kumarashutosh34169@gmail.com>
2025-08-19 02:08:02 +0530

    zig zag level order trav

---
+++ Tree/PartVI.java
@@ -0,0 +1,48 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class PartVI {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        System.out.println(zigzagLevelOrder(root));
+    }
+
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+            int lvlSize = queue.size();
+            List<Integer> currList = new ArrayList<>();
+
+            for (int i = 0; i < lvlSize; i++) {
+                TreeNode curr = queue.poll();
+                if (curr == null) break;
+
+                if (leftToRight) currList.add(curr.val);
+                else currList.add(0, curr.val);
+
+                if (curr.left != null) queue.add(curr.left);
+                if (curr.right != null) queue.add(curr.right);
+            }
+
+            result.add(currList);
+            leftToRight = !leftToRight;
+        }
+        return result;
+    }
+}
