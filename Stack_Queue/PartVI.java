7f48f09092c0b7580cd94e800ad502207525b388
bravo1goingdark <kumarashutosh34169@gmail.com>
2026-02-11 18:55:15 +0530

    Add prefix/postfix/infix conversion methods

--- Stack_Queue/PartVI.java
+++ Stack_Queue/PartVI.java
@@ -2,12 +2,69 @@ package Stack_Queue;
 
 public class PartVI {
     public static void main(String[] args) {
-        System.out.println(postFixToPrefix("ab+cde+**"));
+
+        System.out.println(postfixToPrefix("ab+cde+**"));
+        System.out.println(postfixToInFix("ab+c+"));
+        System.out.println(prefixToInfix("+A*BC"));
+        System.out.println(prefixToPostfix("/-AB*+DEF"));
     }
-    public static String postFixToPrefix(String input){
+
+    public static String prefixToPostfix(String input) {
         Stack<String> stack = new Stack<>();
 
-        for (char ch : input.toCharArray()){
+        for (int i = input.length() - 1; i >= 0; i--) {
+            char ch = input.charAt(i);
+            if (Character.isLetterOrDigit(ch)) stack.push(String.valueOf(ch));
+            else {
+                String op1 = stack.pop();
+                String op2 = stack.pop();
+                String expr = op1 + op2 + ch;
+
+                stack.push(expr);
+            }
+        }
+        return stack.pop();
+
+    }
+
+    public static String prefixToInfix(String input) {
+        Stack<String> stack = new Stack<>();
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            char ch = input.charAt(i);
+            if (Character.isLetterOrDigit(ch)) stack.push(String.valueOf(ch));
+            else {
+                String op1 = stack.pop();
+                String op2 = stack.pop();
+                String expr = "(" + op1 + ch + op2 + ")";
+
+                stack.push(expr);
+            }
+        }
+        return stack.pop();
+    }
+
+
+    public static String postfixToInFix(String input) {
+        Stack<String> stack = new Stack<>();
+
+        for (char ch : input.toCharArray()) {
+            if (Character.isLetterOrDigit(ch)) stack.push(String.valueOf(ch));
+            else {
+                String op2 = stack.pop();
+                String op1 = stack.pop();
+
+                String expr = "(" + op1 + ch + op2 + ")";
+                stack.push(expr);
+            }
+        }
+        return stack.pop();
+    }
+
+    public static String postfixToPrefix(String input) {
+        Stack<String> stack = new Stack<>();
+
+        for (char ch : input.toCharArray()) {
             if (Character.isLetterOrDigit(ch)) stack.push(String.valueOf(ch));
             else {
                 String op2 = stack.pop();
@@ -17,6 +74,6 @@ public class PartVI {
                 stack.push(expr);
             }
         }
-        return stack.peek();
+        return stack.pop();
     }
 }
