b45612aab304c9cc20a4386b9c93878129d9995a
bravo1goingdark <kumarashutosh34169@gmail.com>
2026-02-12 21:12:55 +0530

    next greater element 1 && 2

---
+++ Stack_Queue/PartVIII.java
@@ -0,0 +1,60 @@
+// https://leetcode.com/problems/next-greater-element-i/description/
+// https://leetcode.com/problems/next-greater-element-ii/description/
+package Stack_Queue;
+
+import java.util.Arrays;
+
+public class PartVIII {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(nextGreaterElementI(new int[]{4, 12, 5, 3, 1, 2, 5, 3, 1, 2, 4, 6})));
+    }
+
+    public static int[] nextGreaterElementI(int[] nums) {
+        int[] nge = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int curr = nums[i];
+
+
+            if (!stack.isEmpty() && stack.peek() <= curr) {
+                while (!stack.isEmpty() && stack.peek() <= curr) {
+                    stack.pop();
+                }
+            }
+
+            if (stack.isEmpty()) nge[i] = -1;
+            else nge[i] = stack.peek();
+
+            stack.push(curr);
+        }
+        return nge;
+    }
+
+    public int[] nextGreaterElementsII(int[] nums) {
+        int[] nge = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 2 * nums.length - 1; i >= 0; i--) {
+            int curr = nums[i % nums.length];
+
+
+            if (!stack.isEmpty() && stack.peek() <= curr) {
+                while (!stack.isEmpty() && stack.peek() <= curr) {
+                    stack.pop();
+                }
+            }
+
+            if (i < nums.length){
+                if (stack.isEmpty()) nge[i] = -1;
+                else nge[i] = stack.peek();
+            }
+
+            stack.push(curr);
+        }
+        return nge;
+
+
+    }
+}
